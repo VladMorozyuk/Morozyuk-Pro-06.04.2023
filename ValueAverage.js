6f24eae1c91c61838909d438a54f1164a8823e57
VladMorozyuk <moroziukvladyslav@gmail.com>
2023-04-15 17:28:28 +0300

    js logic value average

---
+++ ValueAverage.js
@@ -0,0 +1,7 @@
+let num1 = prompt("Введіть перше число:");
+let num2 = prompt("Введіть друге число:");
+let num3 = prompt("Введіть третє число:");
+
+let average = (Number(num1) + Number(num2) + Number(num3)) / 3;
+
+alert("Середнє арифметичне чисел " + num1 + ", " + num2 + " і " + num3 + " дорівнює " + average + ".");
