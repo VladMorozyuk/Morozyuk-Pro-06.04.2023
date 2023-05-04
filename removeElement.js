e4398717a6c05a04ad5492795725b7da083b5c0e
VladMorozyuk <moroziukvladyslav@gmail.com>
2023-05-04 19:22:26 +0300

    remove element

---
+++ removeElement.js
@@ -0,0 +1,9 @@
+function removeElement(array, item) {
+    const index = array.indexOf(item);
+    if (index > -1) {
+        array.splice(index, 1);
+    }
+}
+const array = [1, 2, 3, 4, 5, 6, 7];
+removeElement(array, 4);
+console.log(array);  
