0f65aae21e378a2cb9f304861f1fefdf066535e6
VladMorozyuk <moroziukvladyslav@gmail.com>
2023-05-09 14:03:52 +0300

    recursive function

---
+++ RecursiveExponentiation.js
@@ -0,0 +1,16 @@
+function pow(num, degree) {
+    if (degree === 0) {
+        return 1;
+    } else if (degree === 1) {
+        return num;
+    } else if (degree < 0) {
+        return 1 / pow(num, -degree);
+    } else {
+        return num * pow(num, degree - 1);
+    }
+}
+
+
+
+
+
