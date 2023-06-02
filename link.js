3aaa7ede6f112a379c02ef695c7ac20a3520e866
VladMorozyuk <moroziukvladyslav@gmail.com>
2023-06-02 11:24:05 +0300

    link

---
+++ link.js
@@ -0,0 +1,9 @@
+function redirect() {
+    var url = document.getElementById("urlInput").value.trim();
+
+    if (!url.startsWith("http://") && !url.startsWith("https://")) {
+        url = "http://" + url;
+    }
+
+    window.location.href = url;
+}
